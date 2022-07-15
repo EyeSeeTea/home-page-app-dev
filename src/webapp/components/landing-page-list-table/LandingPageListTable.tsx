f674fcf8de7556d3e4ffd6e7fed0bed0d7ea4fb9
xurxodev <xurxosf@gmail.com>
2022-07-15 09:14:55 +0200

    Add radius and shadow to content column in landings

--- src/webapp/components/landing-page-list-table/LandingPageListTable.tsx
+++ src/webapp/components/landing-page-list-table/LandingPageListTable.tsx
@@ -373,4 +373,7 @@ const StepPreview: React.FC<{
 
 const StyledModalBody = styled(ModalBody)`
     max-width: 600px;
+    border-radius: 18px;
+    box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12),
+        0 5px 5px -3px rgba(0, 0, 0, 0.2);
 `;
