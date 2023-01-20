d04d4b9a737c1ef01340fc5e2fa0fe79f40210a9
deeonwuli <onwulidumebi@gmail.com>
2023-01-20 16:25:54 +0100

    add logout action

--- src/webapp/components/landing-layout/LandingHeader.tsx
+++ src/webapp/components/landing-layout/LandingHeader.tsx
@@ -2,13 +2,14 @@ import BackIcon from "@material-ui/icons/ArrowBack";
 import HomeIcon from "@material-ui/icons/Home";
 import SettingsIcon from "@material-ui/icons/Settings";
 import AboutIcon from "@material-ui/icons/Info";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import React from "react";
 import styled from "styled-components";
 import i18n from "../../../locales";
 import { Tooltip, TooltipWrapper } from "../tooltip/Tooltip";
 import { Grid } from "@material-ui/core";
 
-export const LandingHeader: React.FC<LandingHeaderProps> = ({ onGoHome, onGoBack, onSettings, onAbout }) => {
+export const LandingHeader: React.FC<LandingHeaderProps> = ({ onGoHome, onGoBack, onSettings, onAbout, onLogout }) => {
     return (
         <Grid container direction="row" justifyContent="space-between" alignItems="center">
             <div>
@@ -33,6 +34,13 @@ export const LandingHeader: React.FC<LandingHeaderProps> = ({ onGoHome, onGoBack
                     </HomeButton>
                 ) : null}
             </div>
+            <div>
+                {onLogout ? (
+                    <HomeButton text={i18n.t("Logout")} placement={"left"}>
+                        <ExitToAppIcon onClick={onLogout} />
+                    </HomeButton>
+                ) : null}
+            </div>
         </Grid>
     );
 };
@@ -42,6 +50,7 @@ export interface LandingHeaderProps {
     onSettings?: () => void;
     onAbout?: () => void;
     onGoBack?: () => void;
+    onLogout?: () => void;
 }
 
 const HomeButton = styled(Tooltip)`
