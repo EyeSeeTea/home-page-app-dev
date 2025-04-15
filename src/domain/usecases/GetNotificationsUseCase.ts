ed1a0ff5c572084d3a2f777b1e5f6b39d040b564
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-04-15 13:26:28 +0800

    fix: sort notifs by created desc

--- src/domain/usecases/GetNotificationsUseCase.ts
+++ src/domain/usecases/GetNotificationsUseCase.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 import { NotificationRepository } from "../repositories/NotificationRepository";
 import { FutureData } from "../types/Future";
 import { Notification } from "../entities/Notification";
@@ -6,6 +8,10 @@ export class GetNotificationsUseCase {
     constructor(private notificationRepository: NotificationRepository) {}
 
     public execute(): FutureData<Notification[]> {
-        return this.notificationRepository.list(null);
+        return this.notificationRepository.list(null).map(notifications =>
+            _(notifications)
+                .sortBy(notification => notification.createdAt, "desc")
+                .value()
+        );
     }
 }
