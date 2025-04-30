32ccd3636b23cd8702be8e029c68ae5577df3722
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-04-30 12:26:14 +0800

    refactor: save usecase - no need to rebuild notification collection in

--- src/domain/usecases/SaveNotificationsUseCase.ts
+++ src/domain/usecases/SaveNotificationsUseCase.ts
@@ -1,5 +1,3 @@
-import _ from "lodash";
-
 import { NotificationRepository } from "../repositories/NotificationRepository";
 import { FutureData } from "../types/Future";
 import { Notification } from "../entities/Notification";
@@ -12,32 +10,12 @@ export class SaveNotificationsUseCase {
     public execute(notifications: Notification[]): FutureData<void> {
         return this.userRepository
             .getCurrentUser()
-            .flatMap(user => this.fetchAndUpdateNotifications(user, notifications));
+            .flatMap(user => this.updateAndSaveNotifications(user, notifications));
     }
 
-    private fetchAndUpdateNotifications(user: User, notifications: Notification[]): FutureData<void> {
-        return this.notificationRepository
-            .list(null)
-            .map(allNotifications => this.updateNotifications(allNotifications, notifications, user))
-            .flatMap(updatedAndNew => this.notificationRepository.save(updatedAndNew));
-    }
-
-    private updateNotifications(
-        allNotifications: Notification[],
-        notifications: Notification[],
-        user: User
-    ): Notification[] {
-        //assume current user saving/updating the notification has read the notification
+    private updateAndSaveNotifications(user: User, notifications: Notification[]): FutureData<void> {
+        //assume user saving the notification has read it
         const readNotifications = notifications.map(notification => notification.markAsRead(user));
-
-        const notificationMap = _.keyBy(readNotifications, notification => notification.id);
-        const updatedNotifs = allNotifications
-            .filter(notification => !!notificationMap[notification.id])
-            .map(notificationToUpdate =>
-                Notification.create({ ...notificationToUpdate, ...notificationMap[notificationToUpdate.id] })
-            );
-        const newNotifs = _.differenceBy(readNotifications, updatedNotifs, notification => notification.id);
-
-        return [...updatedNotifs, ...newNotifs];
+        return this.notificationRepository.save(readNotifications);
     }
 }
