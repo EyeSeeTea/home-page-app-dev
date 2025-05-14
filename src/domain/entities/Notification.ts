6aa4b67e516bd344b625722c6aa3f9b8b39dde85
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-05-14 15:09:50 +0800

    remove unnecessary methods from model

--- src/domain/entities/Notification.ts
+++ src/domain/entities/Notification.ts
@@ -26,35 +26,12 @@ export class Notification extends Struct<NotificationAttributes>() {
         const alreadyRead = this.readBy.some(read => read.id === user.id);
         if (alreadyRead) return this;
 
-        const newReadBy = [
-            ...this.readBy,
-            { date: new Date(), id: user.id, name: user.name },
-        ];
+        const newReadBy = [...this.readBy, { date: new Date(), id: user.id, name: user.name }];
 
         return this._update({ readBy: newReadBy });
     }
 
     isReadBy(userId: string): boolean {
         return this.readBy.some(read => read.id === userId);
-    }
-
-    addRecipients(users: NamedRef[] = [], userGroups: NamedRef[] = []): Notification {
-        return this._update({
-            recipients: {
-                ...this.recipients,
-                users: [...this.recipients.users, ...users],
-                userGroups: [...this.recipients.userGroups, ...userGroups],
-            },
-        });
-    }
-
-    static createNew(content: string, recipients: NotificationRecipients): Notification {
-        return this.create({
-            id: crypto.randomUUID(),
-            content,
-            createdAt: new Date(),
-            readBy: [],
-            recipients,
-        });
     }
 }
