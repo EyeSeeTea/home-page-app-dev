19f79227a3636b8e13d152b03bc9be58537e6f9f
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-05-16 15:40:53 +0800

    feat: aider - create data layer
    ---
    
    Readonly:                                 ../../.aider/CONVENTIONS.md                             src/data/repositories/NotificationDefaultRepository.ts
    src/domain/entities/NotificationConfig.ts src/domain/repositories/NotificationConfigRepository.ts src/domain/types/Future.
    
    > Create `NotificationConfigDefaultRepository` in data/repositories. Follow `NotificationDefaultRepository`

---
+++ src/data/repositories/NotificationConfigDefaultRepository.ts
@@ -0,0 +1,39 @@
+import { NotificationConfig } from "../../domain/entities/NotificationConfig";
+import { NotificationConfigRepository } from "../../domain/repositories/NotificationConfigRepository";
+import { FutureData } from "../../domain/types/Future";
+import { Config } from "../entities/Config";
+import { StorageClient } from "../clients/storage/StorageClient";
+import { Namespaces } from "../clients/storage/Namespaces";
+import { Future } from "../../domain/types/Future";
+import { fromPromise } from "../api-futures";
+import { DataStoreStorageClient } from "../clients/storage/DataStoreStorageClient";
+
+export class NotificationConfigDefaultRepository implements NotificationConfigRepository {
+    private storageClient: StorageClient;
+    private readonly namespace = Namespaces.NOTIFICATIONS;
+    private readonly key = "config";
+
+    constructor(private config: Config) {
+        this.storageClient = new DataStoreStorageClient({
+            type: "global",
+            instance: config.instance,
+            namespace: this.namespace,
+        });
+    }
+
+    get(): FutureData<NotificationConfig> {
+        return fromPromise(this.storageClient.getObject<NotificationConfig>(this.key))
+            .flatMapError(error => {
+                console.error("Failed to get notification config:", error);
+                return Future.error("Could not retrieve notification config from storage");
+            })
+            .map(config => config ?? { permissions: { users: [], userGroups: [] } });
+    }
+
+    save(config: NotificationConfig): FutureData<void> {
+        return fromPromise(this.storageClient.saveObject(this.key, config)).flatMapError(error => {
+            console.error("Failed to save notification config:", error);
+            return Future.error("Could not save notification config to storage");
+        });
+    }
+}
