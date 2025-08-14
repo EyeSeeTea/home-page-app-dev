1ec0f15fae085f07966b27441b0a90d2115c971b
gqcorneby <185756521+gqcorneby@users.noreply.github.com>
2025-08-14 10:07:08 +0800

    create separate usecase to save mark as read

--- src/domain/usecases/ReadUserNotificationUseCase.ts
+++ src/domain/usecases/ReadUserNotificationUseCase.ts
@@ -1,21 +1,12 @@
 import { NotificationRepository } from "../repositories/NotificationRepository";
-import { Future, FutureData } from "../types/Future";
+import { FutureData } from "../types/Future";
 import { Notification } from "../entities/Notification";
 import { User } from "../entities/User";
 
-export class SaveNotificationsUseCase {
+export class ReadUserNotificationUseCase {
     constructor(private notificationRepository: NotificationRepository) {}
 
     public execute(notifications: Notification[], user: User): FutureData<void> {
-        const allErrors = notifications.flatMap(notification => {
-            const errors = notification.validate({ user });
-            return errors.map(err => err.message);
-        });
-
-        if (allErrors.length > 0) {
-            return Future.error(allErrors.join("\n"));
-        }
-
         // assume user saving the notification has read it
         const updatedNotifs = Notification.readAllNotifications(notifications, user);
         return this.notificationRepository.save(updatedNotifs);
